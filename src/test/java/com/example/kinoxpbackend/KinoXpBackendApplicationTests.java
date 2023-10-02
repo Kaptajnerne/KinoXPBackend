b4dc5c10376e5c36e8a99391e1c4190a12dfeca3
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-10-02 09:30:45 +0200

    Packages included and directories made

---
+++ src/test/java/com/example/kinoxpbackend/KinoXpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinoxpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
