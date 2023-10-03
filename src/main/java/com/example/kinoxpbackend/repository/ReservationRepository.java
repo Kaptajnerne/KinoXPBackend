1abf72181420f588acd5d019a20c5012b1b08df2
kjartanleander <Kjar0321@stud.kea.dk>
2023-10-03 15:50:57 +0200

    Jeg har oprettet controllere til Reservation og Theater

--- src/main/java/com/example/kinoxpbackend/repository/ReservationRepository.java
+++ src/main/java/com/example/kinoxpbackend/repository/ReservationRepository.java
@@ -1,6 +1,5 @@
 package com.example.kinoxpbackend.repository;
 
-import com.example.kinoxpbackend.model.Movie;
 import com.example.kinoxpbackend.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
