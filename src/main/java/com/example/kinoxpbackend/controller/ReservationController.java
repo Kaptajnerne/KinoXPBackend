ee8408c329baf95eacb4130e76f0e37a9b6c9de2
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-10-09 11:51:11 +0200

    Create reservation with agelimit and price calculation

--- src/main/java/com/example/kinoxpbackend/controller/ReservationController.java
+++ src/main/java/com/example/kinoxpbackend/controller/ReservationController.java
@@ -1,7 +1,10 @@
 package com.example.kinoxpbackend.controller;
 
 import com.example.kinoxpbackend.model.Reservation;
+import com.example.kinoxpbackend.model.SeatShowtime;
 import com.example.kinoxpbackend.repository.ReservationRepository;
+import com.example.kinoxpbackend.repository.SeatShowtimeRepository;
+import com.example.kinoxpbackend.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,12 @@ public class ReservationController {
     @Autowired
     private ReservationRepository reservationRepository;
 
+    @Autowired
+    private SeatShowtimeRepository seatShowtimeRepository;
+
+    @Autowired
+    private ReservationService reservationService;
+
     @GetMapping()
     public ResponseEntity<List<Reservation>> findAll() {
         List<Reservation> reservations = reservationRepository.findAll();
@@ -30,9 +39,15 @@ public class ReservationController {
         return reservation.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping()
-    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
-        Reservation createdReservation = reservationRepository.save(reservation);
+    @PostMapping("/create")
+    public ResponseEntity<?> createReservation(@RequestBody Reservation reservation, @RequestParam List<Integer> seatShowtimeIds) {
+        List<SeatShowtime> selectedSeatShowtimes = seatShowtimeRepository.findAllById(seatShowtimeIds);
+        boolean isAgeWithinLimit = reservationService.isAgeWithinLimit(reservation.getAge(), selectedSeatShowtimes);
+
+        if (!isAgeWithinLimit) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Hey you little shit, your not old enough, so pack your shit and get going.");
+        }
+        Reservation createdReservation = reservationService.createReservation(reservation, selectedSeatShowtimes);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdReservation);
     }
 
--- src/main/java/com/example/kinoxpbackend/service/ReservationService.java
+++ src/main/java/com/example/kinoxpbackend/service/ReservationService.java
@@ -5,22 +5,46 @@ import com.example.kinoxpbackend.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
+
 @Service
 public class ReservationService {
 
     @Autowired
     ReservationRepository reservationRepository;
 
-    //If the user making a reservation is not within the age limit they can't make a reservation.
-    //maybe make age limit its own method and combine into createReservation method
-    public Reservation createReservation(Reservation reservation, SeatShowtime seatShowtime) {
+    public Reservation createReservation(Reservation reservation, List<SeatShowtime> selectedSeatShowtimes) {
+        reservation.setName(reservation.getName());
+        reservation.setAge(reservation.getAge());
+        reservation.setEmail(reservation.getEmail());
+        reservation.setSeatShowtimes(new HashSet<>(selectedSeatShowtimes));
+        int totalPrice = calculatePrice(selectedSeatShowtimes);
+        reservation.setFullPrice(totalPrice);
+
+        if (!isAgeWithinLimit(reservation.getAge(), selectedSeatShowtimes)) {
+            throw new IllegalArgumentException("Hey you little shit, your not old enough, so pack your shit and get going.");
+        }
+        reservation = reservationRepository.save(reservation);
+
         return reservation;
     }
 
-    //Calculate the cost of all the chosen seats
-    public int calculatePrice () {
-        int fullPrice = 0;
-        return fullPrice;
+    public int calculatePrice(List<SeatShowtime> selectedSeatShowtimes) {
+        int totalPrice = 0;
+        for (SeatShowtime seatShowtime : selectedSeatShowtimes) {
+            totalPrice += seatShowtime.getPrice();
+        }
+        return totalPrice;
     }
 
+    public boolean isAgeWithinLimit(int age, List<SeatShowtime> selectedSeatShowtimes) {
+        for (SeatShowtime seatShowtime : selectedSeatShowtimes) {
+            int ageLimit = seatShowtime.getShowTime().getMovie().getAgeLimit();
+            if (age < ageLimit) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
